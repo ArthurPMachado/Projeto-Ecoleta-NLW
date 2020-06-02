f18cb7db7a3146a3624ebfe21149edd43808390c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-02 19:20:06 -0300

    Add devDependencies

--- server/source/server.ts
+++ server/source/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const server = express();
 
-server.get('/users', () => {
-    console.log('Listagem de usuários')
+server.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'Arthur',
+        'Carol',
+        'Felipe',
+        'Karen',
+        'Giovana'
+    ]);
 });
 
 server.listen(3000);
