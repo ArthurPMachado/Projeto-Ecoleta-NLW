5ba07e9d35628e5a103d7309b81d7281c65fde43
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-03 13:28:27 -0300

    Create some examples with routes

--- server/source/server.ts
+++ server/source/server.ts
@@ -1,17 +1,41 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const server = express();
+
+// Rota: endereço completo da requisição
+// Recursos: qual entidade estamos acessando do sistema
+
+const users = [
+    'Arthur',
+    'Carol',
+    'Felipe',
+    'Karen',
+    'Giovana'
+];
 
 server.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    response.json([
-        'Arthur',
-        'Carol',
-        'Felipe',
-        'Karen',
-        'Giovana'
-    ]);
+    response.json(users);
 });
 
-server.listen(3000);
+server.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+server.post('/users', (request, response) => {
+    const user = {
+        name: 'Arthur',
+        surname: 'Machado',
+        email: 'arthurpereiramachado01@gmail.com',
+        age: 20
+    }
+
+    return response.json(user);
+})
+
+server.listen(3300);
