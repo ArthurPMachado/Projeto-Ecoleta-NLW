1bdbe4ba301535146502890cf1c8be1eb1dc1700
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-03 14:52:55 -0300

    Start API routes

--- server/source/server.ts
+++ server/source/server.ts
@@ -1,46 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const server = express();
 
 server.use(express.json());
 
-const users = [
-    'Arthur',
-    'Carol',
-    'Felipe',
-    'Karen',
-    'Giovana'
-];
-
-server.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filterUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filterUsers);
-});
-
-server.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-server.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        surname: data.surname,
-        email: data.email,
-        age: data.age
-    }
-
-    return response.json(user);
-})
+server.use(routes);
 
 server.listen(3300);
