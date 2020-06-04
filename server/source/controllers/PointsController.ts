4a91e041fa7a57023652732777e6253b93172d36
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-04 16:06:18 -0300

    Feat: list items in points

--- server/source/controllers/PointsController.ts
+++ server/source/controllers/PointsController.ts
@@ -58,6 +58,11 @@ export default class PointsController {
             return response.status(400).json({ message: 'Point not found.'});
         }
 
-        return response.json(point);
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.titulo');
+
+        return response.json({ point, items });
     }
 }
