881dac1e102da51242268a061bc351279abb26b5
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-04 19:56:16 -0300

    Create examples to learn React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+// Import Components
+import Header from './components/Header';
+
+export default function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+        <Header />
+
+        <h1>Contéudo da aplicação</h1>
+    </div>
   );
 }
 
-export default App;
+
