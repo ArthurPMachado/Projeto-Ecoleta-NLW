f9d09a301eb4502caf76419605ee88c9bf859393
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-04 11:51:48 -0300

    Create list items and insert points

--- server/source/routes.ts
+++ server/source/routes.ts
@@ -1,9 +1,43 @@
-import express from 'express';
+import express, { request } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*'); 
+
+    const serilizedItems = items.map(item => {
+        return {
+            title: item.titulo,
+            image_url: `http://localhost:3300/uploads/${item.imagem}`
+        }
+    })
+
+    return response.json(serilizedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        nome,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        cidade,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-tmp',
+        nome,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        cidade,
+        uf
+    })
 });
 
 export default routes;
