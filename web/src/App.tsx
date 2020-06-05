4510cf5f5773f2350443cdb1e49b340f8ede0170
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-05 12:20:36 -0300

    Feat: Add css with Roboto and Ubuntu fonts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // Import Components
-import Header from './components/Header';
+
 
 export default function App() {
-  return (
-    <div>
-        <Header />
-
-        <h1>Contéudo da aplicação</h1>
-    </div>
-  );
+    return (
+        <h1>Hello World</h1>
+    );
 }
 
 
