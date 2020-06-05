52d41c82a7ffbeae95ee66ba742a5fff481ecf22
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-05 18:22:21 -0300

    Finish map adition

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -52,7 +52,12 @@ const CreatePoint = () => {
                 </fieldset>
 
                 <Map center={[-23.5267829, -46.5451525]} zoom={15}>
-                    <TileLayer />
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+
+                    <Marker position={[-23.5267829, -46.5451525]}/>
                 </Map>
                 
                 <fieldset>
