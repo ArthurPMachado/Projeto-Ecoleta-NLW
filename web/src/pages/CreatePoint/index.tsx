186baf4db670be7812d3063f47a090de48458228
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-05 17:43:07 -0300

    Finish page structure

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -18,8 +18,102 @@ const CreatePoint = () => {
                     Voltar para home
                 </Link>
             </header>
+            <form >
+                <h1>Cadastro do <br/> do ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="idName">Nome da entidade</label>
+                        <input type="text" 
+                               name="name" 
+                               id="idName"/>
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="idEmail">E-mail</label>
+                            <input type="email" 
+                                name="email" 
+                                id="idEmail"/>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="idWhats">Whatsapp</label>
+                            <input type="text" 
+                                name="whats" 
+                                id="idWhats"/>
+                        </div>
+                    </div>
+                </fieldset>
+
+                
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço do mapa</span>
+                    </legend>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="idUf">Estado (UF)</label>
+                            <select name="uf" id="idUf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="idCidade">Cidade</label>
+                            <select name="cidade" id="idCidade">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3300/uploads/oleo.svg" alt="Imagem de óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
         </div>
     );
 }
 
-export default CreatePoint
+export default CreatePoint;
