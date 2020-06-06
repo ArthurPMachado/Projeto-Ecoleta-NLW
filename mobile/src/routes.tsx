9734dc3793f2786638e8ba3c3b6f6ed3e476527a
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-06 16:45:43 -0300

    Feat: start pages navigation

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Import pages
+import Home from './pages/Home';
+import Points from './pages/Points';
+import Detail from './pages/Detail';
