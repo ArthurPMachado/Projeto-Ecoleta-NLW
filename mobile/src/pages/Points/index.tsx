f664e6bd88737d9edbb1371e6011ac1f49cb645f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-07 02:11:53 -0300

    Feat: filter points by city, uf and items

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, Alert } from 'react-native';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 import api from '../../services/api';
@@ -22,8 +22,16 @@ interface Point {
   longitude: number,
 }
 
+interface Params {
+  uf: string,
+  cidade: string
+}
+
 const Points = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
 
   const [items, setItems] = useState<Item[]>([]);
   const [points, setPoints] = useState<Point[]>([]);
@@ -62,14 +70,14 @@ const Points = () => {
   useEffect(() => {
     api.get('points', {
       params: {
-        cidade: 'São Paulo',
-        uf: 'SP',
-        items: [1, 2, 6]
+        cidade: routeParams.cidade,
+        uf: routeParams.uf,
+        items: selectedItems
       }
     }).then(response => {
       setPoints(response.data);
     });
-  });
+  }, [selectedItems]);
 
   function handleNavigateBack() {
     navigation.goBack();
