6ff981d6a645017cd686a30c09354544d483e0dc
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-06-07 04:21:14 -0300

    Finish dropzone feature

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -1,20 +1,42 @@
-import React, {useCallback} from 'react'
-import { useDropzone } from 'react-dropzone'
+import React, { useCallback, useState} from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+// Import Assets
+import './styles.css';
 
 const Dropzone = () => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
+    const file = acceptedFiles[0];
+
+    const fileUrl = URL.createObjectURL(file);
+
+    setSelectedFileUrl(fileUrl);
   }, []);
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+  const {getRootProps, getInputProps} = useDropzone({
+    onDrop,
+    accept: 'image/*'
+  });
+
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*"/>
+
+      { selectedFileUrl 
+          ? <img src={selectedFileUrl} alt="Thumbnail do ponto de coleta"/>
+          : (
+              <p>
+                <FiUpload />
+                Imagem do estabelecimento
+              </p>
+
+          )
       }
+
     </div>
   );
 }
